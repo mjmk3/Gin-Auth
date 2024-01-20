136933f59bd2caa94b1c34ce465e7fb5d456ae7c
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-21 00:57:01 +0300

    Gin Authentication

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	app := gin.Default()
+
+	route := app
+	isValidated := true
+
+	route.GET("/", func(ctx *gin.Context) {
+
+		if !isValidated {
+			ctx.AbortWithStatusJSON(400, gin.H{
+				"message": "Bad Request",
+			})
+
+			return
+		}
+
+		ctx.JSON(200, gin.H{
+			"hello": "Golang",
+		})
+	})
+
+	app.Run(":8999")
+}
